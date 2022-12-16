bd3498c3d62924656d8bdc4fe73b9ca1cf48921b
Zavarka47 <pazuzu1995@gmail.com>
2022-12-16 23:56:42 +0300

    Done exercise 2.1

--- src/Car.java
+++ src/Car.java
@@ -1,21 +1,26 @@
 public class Car {
-    //марка (brand),
-    //модель (model),
-    //объем двигателя в литрах (engineVolume),
-    //цвет кузова (color),
-    //год производства (year),
-    //страна сборки (country)
-    String brand;
-    String model;
-    double engineVolume;
-    String color;
-    int year;
-    String country;
 
-    Car (String brand, String model, double engineVolume, String color, int year, String country){
+    private final String brand;
+    private final String model;
+    private double engineVolume;
+    private String color;
+    private final int year;
+    private final String country;
+
+
+    private String transmission;
+    private final String bodyType;
+    private String registrationNo;
+    private final int quantityOfSeats;
+    private String rubber;
+
+
+
+    public Car (String brand, String model, double engineVolume, String color, int year, String country,
+         String transmission, String bodyType, String registrationNo, int quantityOfSeats, String rubber){
         this.brand = brand;
         if (brand == null || brand.isBlank()) {
-            this.brand = "default";}
+            brand = "default";}
         this.model = model;
         this.engineVolume = engineVolume;
         if (engineVolume <= 0) {
@@ -25,10 +30,25 @@ public class Car {
             color = "white";}
         this.year = year;
         if (year <= 0) {
-            this.year = 2000;}
+            year = 2000;}
         this.country = country;
+
+        this.transmission = transmission;
+        this.bodyType = bodyType;
+        this.registrationNo = registrationNo;
+        this.quantityOfSeats = quantityOfSeats;
+        this.rubber = rubber;
+        checkString(transmission);
+        checkString(bodyType);
+        checkString(registrationNo);
+        checkInt(quantityOfSeats);
+        checkString(rubber);
     }
-// Методы Get
+
+
+
+
+    // Методы Get
     public String getBrand() {
         return brand;
     }
@@ -48,8 +68,36 @@ public class Car {
         return country;
     }
 
+    public String getTransmission() {return transmission;}
+    public String getBodyType() {return bodyType;}
+    public String getRegistrationNo() {return registrationNo;}
+    public int getQuantityOfSeats() {return quantityOfSeats;}
+    public String getRubber() {return rubber;}
+
+
+// Методы Set
+    public void setEngineVolume(double engineVolume) {this.engineVolume = engineVolume;}
+    public void setColor(String color) {this.color = color;}
+    public void setTransmission(String transmission) {this.transmission = transmission;}
+    public void setNo(String no) {this.registrationNo = no;}
+    public void setRubber(String summerRubber) {this.rubber = summerRubber;}
+
+
     @Override
     public String toString() {
-        return  "brand: " + brand + "; model: " + model + "; engineVolume: " + engineVolume + "; color: " + color + "; year: " + year + "; country: " + country;
+        return  "brand: " + brand + "; model: " + model + "; engineVolume: " + engineVolume + "; color: " + color + "; year: " + year + "; country: " + country;}
+
+    private void checkString (String string) {
+        if (string == null || string.isBlank()){
+            throw new IllegalArgumentException("Информация не указана");
+        } else {
+            string = "default";}
+    }
+    private void checkInt (int i) {
+        if (i < 2){
+            throw new IllegalArgumentException("Информация не указана");
+        } else {
+            i = 2;
+        }
     }
 }
--- src/CarService.java
+++ src/CarService.java
@@ -1,12 +1,20 @@
+import java.time.LocalDate;
+
 public class CarService {
     public static void printCars(Car[] cars) {
         for (int i = 0; i < cars.length; i++) {
-            System.out.println("brand: " + addSpaceBrand(cars[i].getBrand(),cars) +
-                    "; model: " + addSpaceModel(cars[i].getModel(),cars) +
+            System.out.println("brand: " + addSpaceBrand(cars[i].getBrand(), cars) +
+                    "; model: " + addSpaceModel(cars[i].getModel(), cars) +
                     "; engineVolume: " + cars[i].getEngineVolume() +
-                    "; color: " + addSpaceColor(cars[i].getColor(),cars) +
+                    "; color: " + addSpaceColor(cars[i].getColor(), cars) +
                     "; year: " + cars[i].getYear() +
-                    "; country: " + addSpaceCountry(cars[i].getCountry(),cars));
+                    "; country: " + addSpaceCountry(cars[i].getCountry(), cars));
+            System.out.println("transmission: " + addSpaceTransmission(cars[i].getTransmission(), cars) +
+                    "; body type: " + addSpaceBodyType(cars[i].getBodyType(), cars) +
+                    "; registration No.: " + cars[i].getRegistrationNo() +
+                    "; quantity of seats: " + cars[i].getQuantityOfSeats() +
+                    "; rubber: " + addSpaceRubber(cars[i].getRubber(), cars));
+            System.out.println();
         }
     }
 
@@ -31,7 +39,7 @@ public class CarService {
         return maxLengthModel;
     }
 
-    private static int maxLengthColor (Car[]cars) {
+    private static int maxLengthColor(Car[] cars) {
         int maxLengthColor = cars[0].getColor().length();
         for (int i = 0; i < cars.length; i++) {
             if (maxLengthColor < cars[i].getColor().length()) {
@@ -41,7 +49,7 @@ public class CarService {
         return maxLengthColor;
     }
 
-    private static int maxLengthCountry (Car[]cars) {
+    private static int maxLengthCountry(Car[] cars) {
         int maxLengthCountry = cars[0].getCountry().length();
         for (int i = 0; i < cars.length; i++) {
             if (maxLengthCountry < cars[i].getCountry().length()) {
@@ -51,20 +59,39 @@ public class CarService {
         return maxLengthCountry;
     }
 
-    //Методы addSpase
-
-    private static String addSpaceBrand (String brand, Car[]cars) {
-        int max = maxLengthBrand(cars);
-        String fullBrand = brand;
-        if (brand.length() < max) {
-            for (int i = 0; i < (max - brand.length()); i++) {
-                fullBrand = fullBrand + ".";
+    private static int maxLengthTransmission(Car[] cars) {
+        int maxLengthTransmission = cars[0].getTransmission().length();
+        for (int i = 0; i < cars.length; i++) {
+            if (maxLengthTransmission < cars[i].getTransmission().length()) {
+                maxLengthTransmission = cars[i].getTransmission().length();
             }
         }
-        return fullBrand + "...";
+        return maxLengthTransmission;
     }
 
-    private static String addSpaceModel(String model, Car[]cars) {
+    private static int maxLengthBodyType(Car[] cars) {
+        int maxLengthBodyType = cars[0].getBodyType().length();
+        for (int i = 0; i < cars.length; i++) {
+            if (maxLengthBodyType < cars[i].getBodyType().length()) {
+                maxLengthBodyType = cars[i].getBodyType().length();
+            }
+        }
+        return maxLengthBodyType;
+    }
+
+    private static int maxLengthRubber(Car[] cars) {
+        int maxLengthRubber = cars[0].getRubber().length();
+        for (int i = 0; i < cars.length; i++) {
+            if (maxLengthRubber < cars[i].getRubber().length()) {
+                maxLengthRubber = cars[i].getRubber().length();
+            }
+        }
+        return maxLengthRubber;
+    }
+
+
+    //Методы addSpase
+    private static String addSpaceModel(String model, Car[] cars) {
         int max = maxLengthModel(cars);
         String fullModel = model;
         if (model.length() < max) {
@@ -75,7 +102,7 @@ public class CarService {
         return fullModel + "...";
     }
 
-    private static String addSpaceColor (String color, Car[]cars) {
+    private static String addSpaceColor(String color, Car[] cars) {
         int max = maxLengthColor(cars);
         String fullColor = color;
         if (color.length() < max) {
@@ -86,7 +113,7 @@ public class CarService {
         return fullColor + "...";
     }
 
-    private static String addSpaceCountry (String country, Car[]cars) {
+    private static String addSpaceCountry(String country, Car[] cars) {
         int max = maxLengthCountry(cars);
         String fullCountry = country;
         if (country.length() < max) {
@@ -95,6 +122,63 @@ public class CarService {
             }
         }
         return fullCountry + "...";
+    }
+
+    private static String addSpaceBrand(String brand, Car[] cars) {
+        int max = maxLengthBrand(cars);
+        String fullBrand = brand;
+        if (brand.length() < max) {
+            for (int i = 0; i < (max - brand.length()); i++) {
+                fullBrand = fullBrand + ".";
+            }
+        }
+        return fullBrand + "...";
+    }
+
+    private static String addSpaceTransmission(String transmission, Car[] cars) {
+        int max = maxLengthTransmission(cars);
+        String fullTransmission = transmission;
+        if (transmission.length() < max) {
+            for (int i = 0; i < (max - transmission.length()); i++) {
+                fullTransmission = fullTransmission + ".";
+            }
+        }
+        return fullTransmission + "...";
+    }
+
+    private static String addSpaceBodyType(String bodyType, Car[] cars) {
+        int max = maxLengthBodyType(cars);
+        String fullBodyType = bodyType;
+        if (bodyType.length() < max) {
+            for (int i = 0; i < (max - bodyType.length()); i++) {
+                fullBodyType = fullBodyType + ".";
+            }
+        }
+        return fullBodyType + "...";
+    }
+
+    private static String addSpaceRubber(String rubber, Car[] cars) {
+        int max = maxLengthRubber(cars);
+        String fullRubber = rubber;
+        if (rubber.length() < max) {
+            for (int i = 0; i < (max - rubber.length()); i++) {
+                fullRubber = fullRubber + ".";
+            }
+        }
+        return fullRubber + "...";
+    }
+
+    public static void changRubber(Car[] cars) {
+        int localMouth = LocalDate.now().getMonthValue();
+        if (localMouth >= 4 && localMouth <= 10) {
+            for (int i = 0; i < cars.length; i++) {
+                cars[i].setRubber("Summer");
+            }
+        } else {
+            for (int i = 0; i < cars.length; i++) {
+                cars[i].setRubber("Winter");
+            }
+        }
     }
 }
 
